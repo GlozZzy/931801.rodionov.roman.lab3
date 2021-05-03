f54ba948e2d13ead05a19a09ba1f65b209819976
Roman Rodionov <kpackl14@gmail.com>
2021-05-03 17:44:41 +0700

    Добавьте файлы проекта.

---
+++ Lab2_3/Controllers/MockupsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2_3.Models;
+
+namespace Lab2_3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            QuizModel qModel = QuizModel.Instance;
+            qModel.Reset();
+            qModel.Start();
+            return View(qModel);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(QuizModel qModel, string action)
+        {
+            qModel = QuizModel.Instance;
+
+            if (Request.Form["userAnswer"] != "")
+            {
+                qModel.userAnswer = Int32.Parse(Request.Form["userAnswer"]);
+                qModel.Check();
+
+                if (action == "Next")
+                {
+                    QuizModel quModel = QuizModel.Instance;
+                    quModel.Start();
+                    return View(quModel);
+                }
+                return RedirectToAction("QuizResult");
+            }
+            else
+            {
+                QuizModel quModel = QuizModel.Instance;
+                return View(quModel);
+            }
+
+        }
+        public IActionResult QuizResult()
+        {
+            QuizModel qModel = QuizModel.Instance;
+            ViewBag.Result = qModel.ListAnswers;
+            ViewData["Total"] = "" + qModel.count;
+            ViewData["Correct"] = "" + qModel.rightcount;
+            return View();
+        }
+    }
+}
---
+++ Lab2_3/Models/QuizModel.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Lab2_3.Models
+{
+    public class QuizModel
+    {
+        public double val1;
+        public double val2;
+
+        string[] ListOperations = { "+", "-", "*", "/" };
+        public string curOper;
+        
+        public double answer;
+        public int userAnswer;
+        public List<string> ListAnswers;
+
+        public int count;
+        public int rightcount;
+
+        public static QuizModel Instance = new QuizModel();
+
+        public void Reset()
+        {
+            count = 0;
+            rightcount = 0;
+            ListAnswers = new List<string>();
+        }
+
+
+        public void Start()
+        {
+            Random rand = new Random();
+            val1 = rand.Next(0, 10);
+            val2 = rand.Next(0, 10);
+            curOper = ListOperations[rand.Next(0, 3)];
+            count++;
+        }
+        
+
+        public void Check()
+        {
+            switch (curOper)
+            {
+                case "+":
+                    answer = val1 + val2;
+                    if (answer == userAnswer)
+                        rightcount++;
+                    ListAnswers.Add("" + val1 + " + " + val2 + " = " + userAnswer);
+                    break;
+                case "-":
+                    answer = val1 - val2;
+                    if (answer == userAnswer)
+                        rightcount++;
+                    ListAnswers.Add("" + val1 + " - " + val2 + " = " + userAnswer);
+                    break;
+                case "*":
+                    answer = val1 * val2;
+                    if (answer == userAnswer)
+                        rightcount++;
+                    ListAnswers.Add("" + val1 + " * " + val2 + " = " + userAnswer);
+                    break;
+                case "/":
+                    if (val2 != 0)
+                    {
+                        answer = val1 / val2;
+                        if (answer == userAnswer)
+                            rightcount++;
+                        ListAnswers.Add("" + val1 + " / " + val2 + " = " + userAnswer);
+                    }
+                    break;
+            }
+        }
+    }
+}
